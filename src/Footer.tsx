48a46ccafb27465a075476693dfd956b08a09120
Alejandro Morales <ale75342mb@gmail.com>
2024-04-17 16:21:21 -0400

    {add_devtools and restartButtonn}

--- src/Footer.tsx
+++ src/Footer.tsx
@@ -1,14 +1,17 @@
-// import { Button } from "@mui/material";
+import { Button } from "@mui/material";
 import { unserQuestionData } from "./hook/useQuestionsData";
+import { useQuestionStore } from "./store/quetions";
 
 export const Footer = () => {
   const { correct, incorrect, unanswered } = unserQuestionData();
+
+  const reset = useQuestionStore((state) => state.reset);
 
   return (
     <footer style={{ marginTop: "16px" }}>
       <strong>{`✅ ${correct} correctas - ❌ ${incorrect} incorrectas - ❓ ${unanswered} sin responder`}</strong>
       <div style={{ marginTop: "16px" }}>
-        {/* <Button onClick={() => reset()}>Resetear juego</Button> */}
+        <Button onClick={() => reset()}>Resetear juego</Button>
       </div>
     </footer>
   );
