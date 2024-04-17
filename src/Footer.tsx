bf349e842032d325051caeadde0fb12c2e81f543
Alejandro Morales <ale75342mb@gmail.com>
2024-04-17 15:17:54 -0400

    {fix_userQuestionData}

--- src/Footer.tsx
+++ src/Footer.tsx
@@ -3,6 +3,7 @@ import { unserQuestionData } from "./hook/useQuestionsData";
 
 export const Footer = () => {
   const { correct, incorrect, unanswered } = unserQuestionData();
+
   return (
     <footer style={{ marginTop: "16px" }}>
       <strong>{`✅ ${correct} correctas - ❌ ${incorrect} incorrectas - ❓ ${unanswered} sin responder`}</strong>
